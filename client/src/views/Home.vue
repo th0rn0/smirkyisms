b009cddd46be32682e706678a93b2102b44b8f7d
th0rn0 <dev@th0rn0.com>
2020-03-17 20:19:29 +0000

    backend and frontend base complete

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="home">
+  <div id="app" class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Smirkyisms"/>
+    {{ info }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+	el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  created () {
+    axios
+      .get('http://localhost:1337/quote')
+      .then(response => (this.info = response.data))
+      .catch(e => {
+				this.errors.push(e)
+			})
   }
 }
 </script>
