71fd0e5e1d6aa88616b1c4f8142e6b095925396e
th0rn0 <dev@th0rn0.com>
2020-03-17 09:43:11 +0000

    WIP

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Home. */
+/* GET api page. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/quote', function(req, res) {
+  res.json(req);
 });
 
 module.exports = router;
