96c96aede026ba88bc017fdf8d55dc96d63a8476
th0rn0 <dev@th0rn0.com>
2020-03-18 00:36:59 +0000

    wip

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app" class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Smirkyisms"/>
+    <Quote msg="Welcome to Smirkyisms"/>
     {{ info }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Quote from '@/components/Quote.vue'
 import axios from 'axios'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Quote
   },
 	el: '#app',
   data () {
