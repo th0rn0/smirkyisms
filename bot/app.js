19d66c5bf8ddbf33698fe1fdfbc0039279255cb3
th0rn0 <dev@th0rn0.com>
2020-03-20 21:08:31 +0000

    checks for message

--- bot/app.js
+++ bot/app.js
@@ -8,6 +8,8 @@ client.login(TOKEN);
 
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`);
+	client.user.setActivity('Eating Pizza Sandwiches');
+
 });
 
 client.on('message', message => {
@@ -16,4 +18,41 @@ client.on('message', message => {
 		message.channel.send('Fair Sik...');
 		return;
 	}
+	if (message.content.toLowerCase().indexOf('smirket pin') != -1) {
+		console.log(message.cleanContent);
+		message.channel.messages.fetch('690645967149727755')
+		.then( message => {
+				console.log(message);
+				console.log('asdasd');
+				message.awaitReactions({ time: 60 })
+				.then(collected => {
+					console.log(collected)
+				});
+		});
+	 	// console.log(message.channel.messages.fetch('690640627532169336'));
+		message.channel.send('Fair Sik...');
+		return;
+	}
 });
+
+async function getMessages(channel, limit = 100) {
+	const messageCount =[];
+  let lastId;
+	client.user.setActivity('Searching in ' + channel.name + '!');
+ 	while (true) {
+    const options = { limit: 1 };
+    if (lastId) {
+      options.before = lastId;
+    }
+    const messages = await channel.fetchMessage(690635718023708729);
+    messages.forEach((message) => {
+    	console.log(message.reactions)
+    });
+  }
+}
+
+
+async function getMessage(channel, id) {
+	const message = await channel.messages.fetch('690640627532169336');
+	return message
+}
